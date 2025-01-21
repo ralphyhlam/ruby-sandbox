94fbe52485fa33d30a7623a43b46fc08c80c7b3a
ralphyhlam <ralphyhlam@uchicago.edu>
2025-01-21 23:45:59 +0000

    Made ruby changes

---
+++ bye_rl.rb
@@ -0,0 +1,1 @@
+pp "talk to you soon"
---
+++ howdy_rl.rb
@@ -0,0 +1,11 @@
+require "./bye_rl.rb"
+require "active_support/all"
+
+pp 10.ordinalize
+pp "the".pluralize    
+pp "you".pluralize
+
+x = gets
+pp x 
+puts x 
+pp x.chomp
